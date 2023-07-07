0414e0816cdb32b6d52d042280570170d43d2e73
Andras Dobreff <dobreffandras@gmail.com>
2023-07-07 15:05:29 +0200

    Show HttpErrors
    Plus disable form for the waiting for the response

--- NgCommerce/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ NgCommerce/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder } from '@angular/forms';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-fetch-data',
@@ -8,7 +9,8 @@ import { FormBuilder } from '@angular/forms';
 })
 export class FetchDataComponent {
   public products: Product[] = [];
-
+  public newProductFormAvailable: boolean = false;
+  public responseErrorMessage = '';
   newProductForm = this.formBuilder.group({
     name: '',
     description: '',
@@ -21,6 +23,9 @@ export class FetchDataComponent {
     private formBuilder: FormBuilder) {
 
     http.get<Product[]>(baseUrl + 'api/products')
+      .pipe(
+        finalize(() => this.newProductFormAvailable = true)
+      )
       .subscribe(
         result => { this.products = result; },
         error => console.error(error));
@@ -28,12 +33,21 @@ export class FetchDataComponent {
 
   public OnSubmit(): void {
     console.log(this.newProductForm.value)
+
+    this.newProductFormAvailable = false;
+    this.responseErrorMessage = '';
+
     this.http.post<Product>(
       this.baseUrl + 'api/products',
       this.newProductForm.value)
+      .pipe(
+        finalize(() => this.newProductFormAvailable = true)
+      )
       .subscribe(
         result => { this.products.push(result); },
-        error => console.error(error));
+        error => {
+          this.responseErrorMessage = error?.error ?? "Oops, something happened." ;
+        });
   }
 }
 

0bc06f465a99af97ccd581a9738da6374bf7cbf8
Andras Dobreff <dobreffandras@gmail.com>
2023-07-07 10:34:42 +0200

    Add form for creating product

--- NgCommerce/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ NgCommerce/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-fetch-data',
@@ -8,10 +9,25 @@ import { HttpClient } from '@angular/common/http';
 export class FetchDataComponent {
   public products: Product[] = [];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Product[]>(baseUrl + 'api/products').subscribe(result => {
-      this.products = result;
-    }, error => console.error(error));
+  newProductForm = this.formBuilder.group({
+    name: '',
+    description: '',
+    category: '',
+  });
+
+  constructor(
+    http: HttpClient,
+    @Inject('BASE_URL') baseUrl: string,
+    private formBuilder: FormBuilder) {
+
+    http.get<Product[]>(baseUrl + 'api/products')
+      .subscribe(
+        result => { this.products = result; },
+        error => console.error(error));
+  }
+
+  public OnSubmit(): void {
+    console.log(this.newProductForm.value)
   }
 }
 

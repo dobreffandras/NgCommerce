8f048ea53bb92498cf6a1b4450f8cb46dc37c5fb
Andras Dobreff <dobreffandras@gmail.com>
2023-07-10 16:07:08 +0200

    Move products component under admin folder

--- NgCommerce/ClientApp/src/app/app.module.ts
+++ NgCommerce/ClientApp/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { ProductsComponent } from './products/products.component';
+import { ProductsComponent } from './admin/products/products.component';
 
 @NgModule({
   declarations: [
